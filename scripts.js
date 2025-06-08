c4ec4d30f1d8ce9a9f6b2eb8c72d6bb2aa6e93fc
Robert Xu <robertxu@outlook.com.au>
2025-06-08 22:55:10 +1000

    first commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   const button = document.getElementById("shake-button");
+//   const image = button.querySelector("img");
+
+//   button.addEventListener("click", () => {
+//     image.classList.add("shake-pop");
+//   });
+// });
