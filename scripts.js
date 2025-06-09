df8e8806c59acfa1771757a9ef53d374adbddc5f
Robert Xu <robertxu@outlook.com.au>
2025-06-09 13:04:24 +1000

    Kevin Chen

--- scripts.js
+++ scripts.js
@@ -1,9 +1,1 @@
 
-// document.addEventListener("DOMContentLoaded", () => {
-//   const button = document.getElementById("shake-button");
-//   const image = button.querySelector("img");
-
-//   button.addEventListener("click", () => {
-//     image.classList.add("shake-pop");
-//   });
-// });
